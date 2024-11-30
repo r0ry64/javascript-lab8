6751cf332ccf6a466535a51e9c7e7d8e58e47204
r0ry64 <rorybeatz@gmail.com>
2024-11-30 02:41:18 -0500

    Add part 2: working with JSON

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const student = {
     courses: ['Algorithms', 'Web Design', 'Tech Read and Writing'],
 
     displayInfo() {
-        return `Name: ${this.name}, Age: ${this.age}`;
+        return `Name: ${this.name}, Age: ${this.age}, Courses: ${this.courses}`;
     }
 };
 
@@ -15,3 +15,12 @@ console.log(`Name: ${student.name}`);
 console.log(`Age: ${student.age}`);
 
 console.log(student.displayInfo());
+
+// Part 2 ------------------------------------
+
+const studentJSON = JSON.stringify(student);
+const newStudentObject = JSON.parse(studentJSON);
+
+console.log('JSON String:', studentJSON);
+console.log('Parsed Object:', newStudentObject);
+console.log('Objects are identical:', JSON.stringify(student) === JSON.stringify(newStudentObject));
