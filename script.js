0a59101b7bd3d6df8bdc08a68beb871db231806b
r0ry64 <rorybeatz@gmail.com>
2024-11-30 02:53:36 -0500

    Add part 3: destructuring

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const student = {
 
 console.log(`Name: ${student.name}`);
 console.log(`Age: ${student.age}`);
-
 console.log(student.displayInfo());
 
 // Part 2 ------------------------------------
@@ -24,3 +23,15 @@ const newStudentObject = JSON.parse(studentJSON);
 console.log('JSON String:', studentJSON);
 console.log('Parsed Object:', newStudentObject);
 console.log('Objects are identical:', JSON.stringify(student) === JSON.stringify(newStudentObject));
+
+// Part 3 ------------------------------------
+
+const { name, courses } = student;
+
+console.log(`Destructured Name: ${name}`);
+console.log(`Destructured Courses: ${courses.join(', ')}`);
+
+const scores = [85, 69, 78, 99];
+const [score1, score2] = scores;
+
+console.log(`First Score: ${score1}, Second Score: ${score2}`);
